7c2f73f8f958c153be27cb6ffd18b05850b0e964
groeneveld <markg729@gmail.com>
2014-09-22 00:39:01 -0500

    Dialog boxes where you can input values for map and world creation! Also a load dialog that doesn't do anything yet.

--- src/levelBuilder/LevelBuilder.java
+++ src/levelBuilder/LevelBuilder.java
@@ -5,86 +5,93 @@ import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FilenameFilter;
 
+import javax.swing.*;
+
 import objects.*;
 import world.SimpleMap;
 import world.SimpleWorld;
 
-//TODO why were block size and cell size different in the test program, shouldn't they be the same?
 public class LevelBuilder {
 	//cell size in pixels
-	static int cellWidth = 20;
-	static int cellHeight = 20;
-	//block size in pixels
-	static int blockWidth = 20;
-	static int blockHeight = 20;
+	private static int cellWidth = 20;
+	private static int cellHeight = 20;
 	//map dimensions in cells
-	static int mapCellHeight = 30;
-	static int mapCellWidth = 40;
+	private static int mapCellHeight;
+	private static int mapCellWidth;
 	//map dimensions in pixels
-	static int mapHeight = mapCellHeight * cellHeight;
-	static int mapWidth = mapCellWidth * cellWidth;
+	private static int mapHeight;
+	private static int mapWidth;
 	//window dimensions in pixels
-	static int windowHeight = 600;
-	static int windowWidth = 600;
-	//TODO list of available objects
-			
-	static SimpleMap m = new SimpleMap(mapCellWidth, mapCellHeight, cellWidth, cellHeight);//TODO map dimensions should be user selectable at init
-	static Cursor cam = new Cursor();
+	private static int windowHeight;
+	private static int windowWidth;
+	
+	private static String levelName;
+	private static SimpleMap m;
+	private static SimpleWorld w;
+	private static Cursor cam = new Cursor();
+	private static Splash splash = new Splash();
 	
 	public static void main(String[] args){
-		m.addSimpleObject(cam, 20, 20);
-		SimpleWorld w = new SimpleWorld(m, windowWidth, windowHeight, "Level Builder"); //TODO might need window dimensions to be created at time of level creation in the future
-		
-		//I don't know what is going on here, it seems to do what I want it to though, which is making the game listen for key presses
-		LevelBuilder LB = new LevelBuilder();
-		w.addKeyListener(LB.new keyEventHandler());
-		w.addKeyListener(cam);
-		w.setCameraStalk(cam);
-		w.start(false);
-		
-		//visual markers to see where the cursor is in comparison to something TODO this is test code
-		m.addSimpleObject(new Solid1(), 0, 0);
-		m.addSimpleObject(new Solid1(), 0, mapHeight-cellHeight);
-		m.addSimpleObject(new Solid1(), mapWidth-cellWidth, mapHeight-cellHeight);
-		m.addSimpleObject(new Solid1(), mapWidth-cellWidth, 0);
-		
-		//Splash screen
-		//list key bindings
+		loadResources();
+		splashWindow();
 	}
 	
-	//loads map file
-	public void load(){	
-		//TODO make sure currently open map is closed first, and maybe ask to save before opening a new one
-		FilenameFilter filter = new FilenameFilter() {
+	private static void loadResources(){
+		//TODO load images and sounds
+	}
+		
+	private static void splashWindow(){
+		//TODO need background image
+		int result = JOptionPane.showOptionDialog(null, null, "Choose!", JOptionPane.DEFAULT_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, new Object[]{"New Map", "Load Existing Map"}, null);
+		if (result == 0) {
+			newMap();
+		}
+		else if (result == 1) {
+			loadMap();
+		}
+		else {
+			System.exit(0);
+		}
+	}
+	
+	private static void loadMap(){	
+		FilenameFilter saveFilter = new FilenameFilter() {
 		    public boolean accept(File directory, String fileName) {
 		        return fileName.endsWith(".sgs");
 		    }
 		};
 		
 		File savesFolder = new File("saves");
-		File[] saves = savesFolder.listFiles(filter);
+		File[] saves = savesFolder.listFiles(saveFilter);
 		if (savesFolder.exists()){
 			if (saves.length > 0){
-				String[] saveNames = savesFolder.list(filter);
-				for (int i = 0; i < saves.length; i++){
-					System.out.println(i + " " + saveNames[i]); //TODO list saves on screen
+//				String[] saveNames = savesFolder.list(saveFilter);
+				String result = (String) JOptionPane.showInputDialog(null, "Which save file would you like to load?",
+						"Choose!", JOptionPane.QUESTION_MESSAGE, null, saves, saves[0]);
+				if (result == null) {
+					splashWindow();
 				}
-				//TODO have user select file they want
-				//TODO load file
+				else {
+					File saveFile = new File(result);
+					//TODO load file
+				}
 			}
 			else {
-				System.out.println("No existing save files"); //TODO change to onscreen message, find out how to do that
+				JOptionPane.showMessageDialog(null, "No existing save files");
+				splashWindow();
 			}
 		}
 		else {
 			savesFolder.mkdir();
-			System.out.println("No existing save files"); //TODO change to onscreen message, find out how to do that
+			JOptionPane.showMessageDialog(null, "No existing save files");
+			splashWindow();
 		}
+		
 	}
 	
-	//saves map file
-	public void save(){
-		FilenameFilter filter = new FilenameFilter() {
+	private void saveMap(){
+		FilenameFilter saveFilter = new FilenameFilter() {
 		    public boolean accept(File directory, String fileName) {
 		        return fileName.endsWith(".sgs");
 		    }
@@ -93,41 +100,88 @@ public class LevelBuilder {
 		File savesFolder = new File("saves");
 		if (!savesFolder.exists())
 			savesFolder.mkdir();
-		File[] saves = savesFolder.listFiles(filter);
+		File[] saves = savesFolder.listFiles(saveFilter);
 		if (saves.length > 0){
-			String[] saveNames = savesFolder.list(filter);
+			String[] saveNames = savesFolder.list(saveFilter);
 			//lists existing files
 			for (int i = 0; i < saves.length; i++){
 				System.out.println(i + " " + saveNames[i]); //TODO list saves on screen
 			}	
 		}
 		//TODO have user select file they want to save over, or a new file
-//			SimpleMapIO IOObj = new SimpleMapIO(path, );
-//			IOObj.writeMap(m);
+//		SimpleMapIO IOObj = new SimpleMapIO(path, );
+//		IOObj.writeMap(m);
+		
 	}
 	
-	//initiates new map
-	public void init(){
-		//TODO routine for selecting map dimensions, and other stuff
+	private static void newMap(){
+		JPanel panel = new JPanel();
+		JTextField ww = new JTextField("800", 5);
+		JTextField wh = new JTextField("600", 5);
+		JTextField mcw = new JTextField("40", 5);
+		JTextField mch = new JTextField("30", 5);
+		JTextField ln = new JTextField("blah", 5);
+		
+		panel.add(new JLabel("Window width in pixels"));
+		panel.add(ww);
+		panel.add(new JLabel("Window height in pixels"));
+		panel.add(wh);
+		panel.add(new JLabel("Map width in cells"));
+		panel.add(mcw);
+		panel.add(new JLabel("Map height in cells"));
+		panel.add(mch);
+		panel.add(new JLabel("Level Name"));
+		panel.add(ln);
+		panel.add(Box.createHorizontalStrut(15));
+		
+		int result = JOptionPane.showConfirmDialog(null, panel, "Map Initialization", JOptionPane.OK_CANCEL_OPTION);
+		if (result == JOptionPane.OK_OPTION) {
+			//TODO could add nice stuff to make it impossible for people to enter non-integers
+			mapCellHeight = Integer.parseInt(mch.getText());
+			mapCellWidth = Integer.parseInt(mcw.getText());
+			windowHeight = Integer.parseInt(wh.getText());
+			windowWidth = Integer.parseInt(ww.getText());
+			levelName = ln.getText();
+			
+			mapWidth = mapCellWidth * cellWidth;
+			mapHeight = mapCellHeight * cellHeight;
+			
+			m = new SimpleMap(mapCellWidth, mapCellHeight, cellWidth, cellHeight);
+			m.addSimpleObject(cam, 20, 20);
+			w = new SimpleWorld(m, windowWidth, windowHeight, "Space Game Level Builder");
+			//I don't know what is going on in the next two lines, it seems to do what I want it to though, which is making the game listen for key presses
+			LevelBuilder LB = new LevelBuilder();
+			w.addKeyListener(LB.new keyEventHandler());
+			w.addKeyListener(cam);
+			w.setCameraStalk(cam);
+			w.start(false);
+			
+			//visual markers to see where the cursor is in comparison to something TODO this is test code
+			m.addSimpleObject(new Solid1(), 0, 0);
+			m.addSimpleObject(new Solid1(), 0, mapHeight-cellHeight);
+			m.addSimpleObject(new Solid1(), mapWidth-cellWidth, mapHeight-cellHeight);
+			m.addSimpleObject(new Solid1(), mapWidth-cellWidth, 0);
+		}
+		else {
+			splashWindow();
+		}
 	}
 	
-	//places object on map
-	public void place(){
+	private void placeObject(){
 		m.addSimpleObject(new Solid1(), cam.getX(), cam.getY());//TODO need to be able to add other types of things
 	}
 	
-	//removes object from map
-	public void remove(){
+	private void removeObject(){
 		m.removeSimpleSolid(cam.getX() / cellWidth, cam.getY() / cellHeight);//TODO still need to be able to remove simpleObjects
 	}
 	
 	//TODO selects object type to place
-	public void selectObjectType(){
+	private void selectObjectType(){
 		
 	}
 	
 	//this class gets called whenever a key is pressed
-	//it then redirects each key to the relevant method or does other relevant stuff
+	//it then redirects each key to the relevant method
 	private class keyEventHandler extends KeyAdapter {
 
 		@Override
@@ -135,29 +189,17 @@ public class LevelBuilder {
 			
 			int key = e.getKeyCode();
 			
-			//place currently selected object at cursor position
 			if (key == KeyEvent.VK_SPACE) {
-				place();
+				placeObject();
 			}
-			//load map dialogue
-			if (key == KeyEvent.VK_L) {
-				load();
-			}
-			//save map dialogue
-			if (key == KeyEvent.VK_S) {
-				save();
-			}
-			//remove object at cursor position
 			if (key == KeyEvent.VK_R) {
-				remove();
+				removeObject();
 			}
-			//changes selected object for placement
 			if (key == KeyEvent.VK_O) {
 				selectObjectType();
 			}
-			//initiates new map
-			if (key == KeyEvent.VK_N) {
-				init();
+			if (key == KeyEvent.VK_S) {
+				saveMap();
 			}
 		}
 	}
